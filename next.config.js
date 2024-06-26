35e9087708863d8c4ff031269b01750885e5ed15
Booteur <berthonge.mibambou.tongo@horizon-tech.tn>
2024-06-26 21:02:17 +0100

    trying something

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    // If you are using the Image component, you might need to configure this
+    // unoptimized: true,
+  },
+  // Optionally, configure basePath and assetPrefix if your site is not hosted at the root of your domain
+  // basePath: '',
+  // assetPrefix: '',
+};
+
+module.exports = nextConfig;
